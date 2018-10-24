c2c35d0c84acd3da43d52514dedaf1cf9e1c6632
Lovdeep <lovdeepgahunia1@gmail.com>
2018-10-24 12:26:21 -0400

    working submit button

--- hangman.js
+++ hangman.js
@@ -1,13 +1,22 @@
-var input = document.querySelector(".wordinput");
-var submitButton = document.querySelector(".submit");
+// const input = document.querySelector(".wordinput");
+const submitWord = document.querySelector(".submit");
+submitWord.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  const value = document.querySelector("input").value;
+  console.log(value);
+});
+// submit word
 
-submitButton.addEventListener("click", submitClick);
+// var input = document.querySelector(".wordinput");
+// var submitButton = document.querySelector(".submit");
 
-function submitClick(evt) {
-  input = [];
-  console.log(evt);
-}
+// submitButton.addEventListener("click", submitClick);
 
+// function submitClick(evt) {
+//   input = [];
+//   console.log(evt);
+// }
+// submitting words
 // function getWord() {
 //   let wordField = document.getElementById("#wordinput").nodeValue;
 //   let words = [];
